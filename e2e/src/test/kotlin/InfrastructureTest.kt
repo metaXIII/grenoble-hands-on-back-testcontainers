a4aac7c15b32169dea65d0dd4ea1f367869d7ea4
Martin Choraine <martin.choraine@gmail.com>
2022-03-02 09:14:40 +0100

    Step 5: use docker compose on functional test

--- e2e/src/test/kotlin/InfrastructureTest.kt
+++ e2e/src/test/kotlin/InfrastructureTest.kt
@@ -5,39 +5,15 @@ import org.junit.jupiter.api.Test
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.containers.MySQLContainer
-import org.testcontainers.containers.Network
-import org.testcontainers.containers.wait.strategy.HttpWaitStrategy
+import org.testcontainers.containers.DockerComposeContainer
+import java.io.File
 import java.util.concurrent.TimeUnit
 
 class InfrastructureTest {
 
   companion object {
-    val network = Network.newNetwork()
-
-    val mysql = MySQLContainer("mysql:8")
-      .withDatabaseName("zenika-weather")
-      .withUsername("zenika")
-      .withPassword("zenika-password")
-      .withNetwork(network)
-      .withNetworkAliases("db")
-    val weatherApi = GenericContainer("7timer:0.0.1-SNAPSHOT")
-      .withNetwork(network)
-      .withNetworkAliases("7timer")
-    val api = GenericContainer("zenika-weather-back:0.0.1-SNAPSHOT")
-      .withEnv("SPRING_DATASOURCE_URL", "jdbc:mysql://db:3306/zenika-weather")
-      .withEnv("SPRING_DATASOURCE_USERNAME", "zenika")
-      .withEnv("SPRING_DATASOURCE_PASSWORD", "zenika-password")
-      .withEnv("WEATHER_URL", "http://7timer:1080")
-      .withNetwork(network)
-      .withNetworkAliases("api")
-    val front = GenericContainer("zenika-weather-front:0.0.1-SNAPSHOT")
-      .withEnv("API_URL", "http://api:8080")
-      .withNetwork(network)
-      .withNetworkAliases("front")
-      .waitingFor(HttpWaitStrategy().forPort(80).forPath("/api/actuator/health"))
-      .withExposedPorts(80)
+    val containers = DockerComposeContainer(File(javaClass.getResource("docker-compose.yml").file))
+      .withExposedService("front_1", 80)
 
     val rootUrl: String
     val driver: WebDriver
@@ -45,12 +21,9 @@ class InfrastructureTest {
     init {
       WebDriverManager.chromedriver().setup()
 
-      mysql.start()
-      weatherApi.start()
-      api.start()
-      front.start()
+      containers.start()
 
-      rootUrl  = "http://localhost:${front.firstMappedPort}"
+      rootUrl  = "http://localhost:${containers.getServicePort("front_1", 80)}"
       driver = ChromeDriver().apply {
         manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS)
       }
