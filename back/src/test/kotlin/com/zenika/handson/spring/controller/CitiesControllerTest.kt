1e89e1a59f053b8d3b4a706ee621c47441b994be
Jeanne Marcadé <marcadej@gmail.com>
2022-03-02 08:47:24 +0100

    Step 1: use generic container with dynamic property registry on integration test on integration test

--- back/src/test/kotlin/com/zenika/handson/spring/controller/CitiesControllerTest.kt
+++ back/src/test/kotlin/com/zenika/handson/spring/controller/CitiesControllerTest.kt
@@ -6,18 +6,43 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.transaction.annotation.Transactional
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @WebMvcTest(controllers = [CitiesController::class])
 @AutoConfigureDataJpa
 @Transactional
+@Testcontainers
 internal class CitiesControllerTest {
 
     @Autowired
     private lateinit var mockMvc: MockMvc
+
+    companion object {
+
+        @Container
+        val mysql = GenericContainer("mysql:8")
+            .withEnv("MYSQL_RANDOM_ROOT_PASSWORD", "true")
+            .withEnv("MYSQL_USER", "zenika")
+            .withEnv("MYSQL_PASSWORD", "zenika-password")
+            .withEnv("MYSQL_DATABASE", "zenika-weather")
+            .withExposedPorts(3306)
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun setupProps(dynamicPropertyRegistry: DynamicPropertyRegistry) {
+            dynamicPropertyRegistry.add("spring.datasource.url") {
+                "jdbc:mysql://localhost:${mysql.firstMappedPort}/zenika-weather"
+            }
+        }
+    }
 
     @Test
     fun `get all cities return 200`() {
